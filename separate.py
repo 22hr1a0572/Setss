8acf5cc7627d1447de8476ae380a7d99f4471008
22hr1a0572 <soujanyam341@gmail.com>
2024-10-15 10:34:33 +0530

    Create separate.py

---
+++ separate.py
@@ -0,0 +1,18 @@
+'''
+ Write a program to get n number of values in separate line for set and print the values with space separation.
+Sample Input:
+5
+3
+1
+4
+5
+2
+'''
+int(input("enter the number  of values")
+for i in range
+set={5,3,1,4,5,2}
+a=int(input("enter number if values"))
+for i in range(a):
+    n=int(input()) 
+print(*set)
+    
