684845919d25733c8e58048103ba40b63f47feeb
22hr1a0572 <soujanyam341@gmail.com>
2024-10-15 10:59:13 +0530

    Create min and max.py

---
+++ min and max.py
@@ -0,0 +1,8 @@
+'''
+Write a program to find the maximum and minimum value of given set values.
+Sample Input:
+1 2 3 4 5
+'''
+set={1,2,3,4,5}
+print("minimum value",min(set))
+print("maximum value",max(set))
